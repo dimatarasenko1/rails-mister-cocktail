d8d03a61a189e36de12b3ce1b8e8454a4dd3148d
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-16 14:08:22 +0100

    added photo uploader when creating new cocktail & display - will display grey if there is no image

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -34,7 +34,7 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :strength, :tagline)
+    params.require(:cocktail).permit(:name, :strength, :tagline, :photo)
   end
 
 end
